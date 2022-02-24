4567e991f420ea72ad3b95ee53bb2bf58a6e5697
irdidolonaku <irdi.dolonaku.simplon@gmail.com>
2022-02-24 13:15:12 +0100

    First Commit

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 $.get("./getProfil.php?name=technet")
 .done( function(data){
 	profil = jQuery.parseJSON(data);
-	$( "#result" ).text(profil.name );
+	$( "#Massimo" ).text(profil.name );
+	$( "#icon" ).attr(src","https://ddragon.leagueoflegends.com/cdn/12.4.1/img/profileicon/3015.png""+profil.profileIconId+".png")
 	console.log(profil);
 	console.log(profil.name);
 });
